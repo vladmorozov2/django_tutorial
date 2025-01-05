a732f22eaace9a1100e91842a0847a760b5f165f
Morozov Vladyslav <vladmorozov@knu.ua>
2025-01-05 21:15:37 +0200

    third part

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index123")]
+app_name = "polls12"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail123"),
+    path("<int:question_id>/results/", views.results, name="result123"),
+    path("<int:question_id>/vote/", views.vote, name="vote123"),
+]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world. You are at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    return HttpResponse(f"Yoa are looking at result of question number {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question number {question_id}")
